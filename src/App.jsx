1d5602cdb88b9ee18cd8c697c6beeefbaef2f36d
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-01-23 12:26:56 +0530

    Add Navbar component and integrate react-icons; update styles and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
        <h1 className='text-3xl font-bold underline'>Hello World</h1>
     </div>
   )
