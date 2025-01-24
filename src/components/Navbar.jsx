c440ef15824697379208f75745b734e181ed823e
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-01-24 06:45:03 +0530

    Add social media icons to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
 import logo from '../assets/kevinRushLogo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+
 const Navbar = () => {
   return (
     <nav className="bg-red-900 mb-20 flex item-center justify-between py-6">
