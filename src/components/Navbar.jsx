97f77549c6caeaa355df5552e14e9b8c0640e4d0
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-01-26 14:33:07 +0530

    Enhance App layout with background gradients and update Navbar logo; add social media icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import logo from '../assets/kevinRushLogo.png'
+import logo from '../assets/1.png'
+import logo2 from '../assets/2.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaTwitterSquare } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex item-center justify-between py-6">
+    <nav className="mb-20 flex item-center justify-between py-6">
         <div className="flex flex-shrink-0 item-center">
-            <img src={logo} alt="logo" />
+            <img src={logo2} alt="logo" height="150px" width="150px"/>
+        </div>
+        <div className="m-8 flex item-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGithub />
+          <FaTwitterSquare />
+          <FaInstagram />
+
         </div>
     </nav>
   )
