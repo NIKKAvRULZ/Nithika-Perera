17003c33dbfa2a9dbc1ed4d60ed398e8e813554d
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-01-28 21:02:52 +0530

    Remove unnecessary whitespace in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 ">
       <h2 className="my-20 text-center text-4xl"> About Me</h2>
+      
     </div>
   )
 }
