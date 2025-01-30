96df5fca98fd7516d27e936de0263ebf243fa7df
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-01-30 17:15:12 +0530

    Enhance Technologies component with improved layout and styling

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,14 @@
+import { Ri24HoursFill, Ri24HoursLine, RiReactjsFill, RiReactjsLine } from "react-icons/ri"
 
 const Technologies = () => {
   return (
-    <div>
-        Technologies
+    <div className="border-b border-neutral-800 pb-24">
+        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+        <div className="flex flex-wrap items-center justify-center gap-4">
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400"/>
+            </div>
+        </div>
     </div>
   )
 }
