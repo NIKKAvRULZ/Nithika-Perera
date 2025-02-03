f9e363d2ac1a8baa7c909a333a9ff2d287b6abb3
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-02-03 20:09:18 +0530

    Update text colors in About and Hero components and refine HERO_CONTENT for clarity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 ">
       <h2 className="my-20 text-center text-4xl"> About 
-        <span className="text-neutral-500"> Me</span></h2>
+        <span className="text-emerald-400"> Me</span></h2>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className=" flex items-center justify-center">
