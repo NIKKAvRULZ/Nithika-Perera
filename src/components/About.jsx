bb88a995fa30f001a572f61f289b10953350a45d
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-02-03 20:02:30 +0530

    Update profile picture format in Hero component and adjust image width in About component for better layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,11 @@ const About = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className=" flex items-center justify-center">
-            <img className="rounded-2xl w-125" src={aboutImg} alt="about" />
+            <img className="rounded-2xl w-100" src={aboutImg} alt="about" />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-            <div className="flex justify-center lg:justify-start">
+            <div className="flex justify-center lg:justify-start ">
               <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
             </div>
           </div>
