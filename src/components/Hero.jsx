8f749b03bfe7f74b7378b7aa8ff1f777b093d17a
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-06-17 09:40:48 +0530

    Add Projects and Contact components; update Experience and Hero components for improved layout and styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
                             Nithika Perera
                         </h1>
-                        <span className="bg-gradient-to-r from-green-500 via-emerald-200 to-emerald-400 bg-clip-text text-4xl tracking-tight text-transparent">
+                        <span className="bg-gradient-to-r from-green-500 via-emerald-200 to-emerald-400 bg-clip-text text-3xl tracking-tight text-transparent">
                             Undergraduate 
                         </span>
                         <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
